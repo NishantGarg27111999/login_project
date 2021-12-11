e36ad15e5d51ad0fabf39721cfb0c7d77b6df771
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2021-12-11 17:01:33 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,33 @@
 let signup = document.getElementById("signup")
 let login = document.getElementById("login");
-if (window.innerWidth < 500) {
+if (window.innerWidth <= 300) {
     function displacelogin() {
         login.style.transform = "translateY(-440px)"
         login.style.opacity = "0";
         
-        signup.style.transform = "translateY(250px)"
+        signup.style.transform = "translateY(340px)"
+        signup.style.opacity = "1";
+
+
+
+    }
+    function displaceSignUp() {
+        login.style.transform = "translateY(0px)"
+        login.style.opacity = "1";
+        // signup.style.display="flex";
+        signup.style.transform = "translateY(0px)"
+        signup.style.opacity = "0"
+
+    }
+
+
+}
+else if(window.innerWidth <= 415) {
+    function displacelogin() {
+        login.style.transform = "translateY(-440px)"
+        login.style.opacity = "0";
+        
+        signup.style.transform = "translateY(285px)"
         signup.style.opacity = "1";
 
 
